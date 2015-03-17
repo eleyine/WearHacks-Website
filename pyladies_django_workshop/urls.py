56327159575bce766dbd6dd1af9b334b9058daca
Eleyine Zarour <eleyine@gmail.com>
2015-03-17 15:27:11 -0400

    super basic view

--- pyladies_django_workshop/urls.py
+++ pyladies_django_workshop/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'pyladies_django_workshop.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'pyladies_django_workshop.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )

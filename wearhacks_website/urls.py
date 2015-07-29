b4ad80538f56d0053db2a9aa69ccbfa873b35992
Eleyine Zarour <eleyine@gmail.com>
2015-07-29 16:16:47 +0400

    remove registration

--- wearhacks_website/urls.py
+++ wearhacks_website/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^register/', include('registration.urls')),
+    # url(r'^register/', include('registration.urls')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.IndexView.as_view(), name='index'),

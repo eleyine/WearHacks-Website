68e445b20a4583bbd327a0f0553e57e060422cc3
Eleyine Zarour <eleyine@gmail.com>
2015-07-31 09:53:28 +0400

    comply less to css if DEBUG (for some reason django compressor doesn't force refresh the CACHE in debug mode)

--- manage.py
+++ manage.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.conf import settings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wearhacks_website.settings")
     # see wearhacks_website/settings/__init__.py
     os.environ.setdefault("APP_ENV", "dev")
 
-    # Normally you should not import ANYTHING from Django directly
-    # into your settings, but ImproperlyConfigured is an exception.
-    from django.core.exceptions import ImproperlyConfigured
+    if settings.DEBUG:
+        error_code = os.system("lesc static/stylesheets/less/styles.less static/stylesheets/css/styles.css")
+        if error_code > 0:
+            print "Warning: Failed to compile LESS files. Make sure you have LESS CSS installed."
 
     from django.core.management import execute_from_command_line
 

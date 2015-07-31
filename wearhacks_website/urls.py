c5674b66f41de57cea2d74b91d8962fc0c182977
Eleyine Zarour <eleyine@gmail.com>
2015-07-31 17:20:20 +0400

    add i18n url patterns

--- wearhacks_website/urls.py
+++ wearhacks_website/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 
 from wearhacks_website import views
 
@@ -7,12 +8,34 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^register/', include('registration.urls')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.IndexView.as_view(), name='index'),
 )
 
+urlpatterns += i18n_patterns(
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/', include('registration.urls')),
+
+)
+
+# Javascript Internationalization Support
+from django.views.i18n import javascript_catalog
+
+js_info_dict = {
+    'packages': (
+            'registration',
+            'static.javascripts',
+        ),
+}
+from django.views.i18n import javascript_catalog
+
+
+urlpatterns = i18n_patterns(
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+) + urlpatterns
+
+# Debug 
 from django.conf import settings
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)

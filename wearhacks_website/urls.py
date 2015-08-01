06b60c42ed8ea59acffc3258703e9f26ce1b14ab
Eleyine Zarour <eleyine@gmail.com>
2015-08-01 14:43:35 +0400

    debug url config

--- wearhacks_website/urls.py
+++ wearhacks_website/urls.py
@@ -6,33 +6,31 @@ from wearhacks_website import views
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     # Examples:
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 )
 
 urlpatterns += i18n_patterns(
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^register/', include('registration.urls')),
-
 )
-
 # Javascript Internationalization Support
 from django.views.i18n import javascript_catalog
 
 js_info_dict = {
     'packages': (
             'registration',
-            'static.javascripts',
+            # 'static.javascripts',
         ),
 }
-from django.views.i18n import javascript_catalog
 
+print javascript_catalog
 
 urlpatterns = i18n_patterns(
-    url(r'^jsi18n/$', javascript_catalog, js_info_dict),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='django.views.i18n.javascript_catalog'),
 ) + urlpatterns
 
 # Debug 

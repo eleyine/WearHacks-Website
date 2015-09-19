999f215c1792ecb96b31f860fe76a6c56ff003cc
Eleyine Zarour <eleyine@gmail.com>
2015-09-19 11:02:32 -0400

    improve stats page

--- registration/urls.py
+++ registration/urls.py
@@ -5,9 +5,10 @@ from registration.views import register, email, confirm, stats
 
 urlpatterns = patterns('',
     url(r'^$', register.SubmitRegistrationView.as_view(), name='register'),
-    url(r'^stats/$', stats.EventStats.as_view(), name='stats'),
     url(r'^confirmation/(?P<order_id>[\dx]+)/$', email.ConfirmationEmailView.as_view(), name='confirmation_email'),
     url(r'^ticket/(?P<order_id>[\dx]+)/$', email.TicketView.as_view(), name='ticket'),
     url(r'^qrcode/(?P<order_id>[\dx]+)/$', email.QRCodeView.as_view(), name='qrcode'),
     url(r'^confirm/(?P<order_id>[\dx]+)/$', confirm.ConfirmRegistrationView.as_view(), name='confirm-registration'),
+    url(r'^stats/$', stats.EventStats.as_view(), name='stats'),
+    url(r'^stats/registrations-by-date/$', stats.get_registration_timeseries, name='registration-timeseries'),
 )
--- registration/views/stats.py
+++ registration/views/stats.py
@@ -14,11 +14,24 @@ from django.utils.translation import pgettext as __
 from collections import defaultdict
 from registration.models import ChargeAttempt, Registration
 
+from jsonview.decorators import json_view
+
 class EventStats(generic.TemplateView):
     template_name = 'registration/stats.html'
 
     def get_context_data(self, **kwargs):
         context = super(EventStats, self).get_context_data(**kwargs)
+
+        context['number_of_registrations'] = Registration.objects.count()
+
+        context['first_time_hackers'] = Registration.objects.filter(is_first_time_hacker=True).count()
+        context['returning_wearhackers'] = Registration.objects.filter(is_returning=True).count()
+
+        context['female'] = Registration.objects.filter(gender='F').count()
+        context['male'] = Registration.objects.filter(gender='M').count()
+        context['other'] = Registration.objects.filter(gender='N').count()
+
+        context['revenue'] = sum([c.amount for c in ChargeAttempt.objects.all()]) / 100.0
 
         context['revenue'] = sum([c.amount for c in ChargeAttempt.objects.all()]) / 100.0
         context['paid_tickets_num'] = sum([1 for r in Registration.objects.all() if r.charge is not None])
@@ -27,13 +40,31 @@ class EventStats(generic.TemplateView):
             Registration.objects.all() if r.food_restrictions != 'None']
 
         context['tshirt_sizes'] = []
-        for tshirt_size in Registration.TSHIRT_SIZE_CHOICES:
-            context['tshirt_sizes'].append({
-                'category': tshirt_size[1],
-                'number': Registration.objects.filter(tshirt_size=tshirt_size[0]).count()
-            })
+        for gender in ['F', 'M', 'N']:
+            gender_tshirt_sizes = {
+                'gender': dict(Registration.GENDER_CHOICES)[gender],
+                'categories': []
+            }
+            for tshirt_size in Registration.TSHIRT_SIZE_CHOICES:
+                gender_tshirt_sizes['categories'].append({
+                    'category': tshirt_size[1],
+                    'number': Registration.objects.filter(tshirt_size=tshirt_size[0], gender=gender).count()
+                })
+            context['tshirt_sizes'].append(gender_tshirt_sizes)
         return context
 
     @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
         return super(EventStats, self).dispatch(request, *args, **kwargs)
+
+@json_view
+def get_registration_timeseries(request, *args, **kwargs):
+    import datetime
+    date_objects = [r.created_at.date() for r in Registration.objects.all()]
+    date_objects.append(datetime.datetime.now().date())
+    date_objects.append(datetime.datetime.now().date() + datetime.timedelta(-3, 0))
+    dates = sorted(list(set(date_objects)))
+    data = []
+    for date in dates:
+        data.append([date, Registration.objects.filter(created_at=date).count()])
+    return data
